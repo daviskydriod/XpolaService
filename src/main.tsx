69e18da24605a4651176602f028026247ed10cd3
nosyradigital8-gif <nosyradigital8@gmail.com>
2026-02-08 01:24:00 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-// FILE PATH: src/main.tsx
-// Place this file at: src/main.tsx
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.tsx'
-import '@/styles/globals.css'
+import App from './App'
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
